7ee6bebcc4525631df02dff8349f2bb20475a80c
dave4506 <me@bydavidsun.com>
2022-02-04 17:37:07 -0600

    ADD v4 only version of nft-swap

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,10 +1,6 @@
-export * from './v3/NftSwapV3';
-export * from './v4/NftSwapV4';
+export * from './v4/types';
+export * from './v4/pure';
 // Default NftSwap is V3. Can opt-into specific versions (V3 or V4) if desired
-export { NftSwapV3 as NftSwap } from './v3/NftSwapV3';
-export * from './v3/types';
-export * from './v3/pure';
+export { NftSwapV4 as NftSwap } from './v4/NftSwapV4';
 export * from '../utils/v3/asset-data';
-// Export contracts for advanced mode
-export * as ExchangeContract from '../contracts/ExchangeContract';
 export * as ExchangeProxy from '../contracts/IZeroEx';
