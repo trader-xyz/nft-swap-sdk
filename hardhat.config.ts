d40ffd657557c9a6ece11e434711f1bfc69e1265
johnrjj <johnrjj@gmail.com>
2022-02-05 02:00:52 -0500

    Adds hardhat for js sdk tests

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+// import "hardhat-jest-plugin";
+// require("hardhat-jest-plugin");
+// import { task } from "hardhat/config";
+import '@nomiclabs/hardhat-waffle';
+// require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  solidity: '0.7.3',
+  networks: {
+    // hardhat: {
+    //   forking: {
+    //     url: "https://eth-mainnet.alchemyapi.io/v2/<key>",
+    //   }
+    // }
+  },
+};
