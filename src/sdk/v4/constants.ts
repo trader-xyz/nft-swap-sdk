ebe25f4548f9c046e8ffd1a8c8b321ef1d998a50
johnrjj <johnrjj@gmail.com>
2022-03-18 00:39:28 -0400

    FIXES v4 erc1155 signature to sign properly with typed data

--- src/sdk/v4/constants.ts
+++ src/sdk/v4/constants.ts
@@ -5,6 +5,8 @@ export const EIP712_DOMAIN_PARAMETERS = [
   { name: 'verifyingContract', type: 'address' },
 ];
 
+export const ERC721ORDER_STRUCT_NAME = 'ERC721Order';
+
 export const ERC721ORDER_STRUCT_ABI = [
   { type: 'uint8', name: 'direction' },
   { type: 'address', name: 'maker' },
@@ -19,6 +21,29 @@ export const ERC721ORDER_STRUCT_ABI = [
   { type: 'Property[]', name: 'erc721TokenProperties' },
 ];
 
+export const EIP1155_DOMAIN_PARAMETERS = [
+  { name: 'name', type: 'string' },
+  { name: 'version', type: 'string' },
+  { name: 'chainId', type: 'uint256' },
+  { name: 'verifyingContract', type: 'address' },
+];
+
+export const ERC1155ORDER_STRUCT_NAME = 'ERC1155Order';
+export const ERC1155ORDER_STRUCT_ABI = [
+  { type: 'uint8', name: 'direction' },
+  { type: 'address', name: 'maker' },
+  { type: 'address', name: 'taker' },
+  { type: 'uint256', name: 'expiry' },
+  { type: 'uint256', name: 'nonce' },
+  { type: 'address', name: 'erc20Token' },
+  { type: 'uint256', name: 'erc20TokenAmount' },
+  { type: 'Fee[]', name: 'fees' },
+  { type: 'address', name: 'erc1155Token' },
+  { type: 'uint256', name: 'erc1155TokenId' },
+  { type: 'Property[]', name: 'erc1155TokenProperties' },
+  { type: 'uint128', name: 'erc1155TokenAmount' },
+];
+
 export const FEE_ABI = [
   { type: 'address', name: 'recipient' },
   { type: 'uint256', name: 'amount' },
@@ -29,5 +54,3 @@ export const PROPERTY_ABI = [
   { type: 'address', name: 'propertyValidator' },
   { type: 'bytes', name: 'propertyData' },
 ];
-
-export const ERC721STRUCT_NAME = 'ERC721Order';
